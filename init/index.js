af5e27e64599b12966f24013aabd6e012a06b113
Subham Kumar Giri <Subhamgiri482@gmail.com>
2025-06-17 11:22:57 +0530

    fixed

--- init/index.js
+++ init/index.js
@@ -1,22 +1,25 @@
-const mongoose = require('mongoose');
-const initdata = require('./data.js');
-const Listing = require("../models/listing");
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
+const MONGO_URL = "mongodb://127.0.0.1:27017/SwaagatInn";
 
 main()
     .then(() => {
-        console.log(' Connected! DB');
+        console.log("connected to DB");
     })
-    .catch ((err) => console.log(err));
+    .catch((err) => {
+        console.log(err);
+    });
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+    await mongoose.connect(MONGO_URL);
 }
 
-const  initDB = async () => {
+const initDB = async () => {
     await Listing.deleteMany({});
-    await Listing.insertMany(initdata.data);
-    console.log("DB Connected");
-}
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+};
 
 initDB();
