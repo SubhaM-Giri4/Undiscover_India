57c165abe9eec2c880c3c1f1b72060cb3ec2b596
Subham Kumar Giri <Subhamgiri482@gmail.com>
2025-06-18 00:03:19 +0530

    style

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-
-const MONGO_URL = "mongodb://127.0.0.1:27017/SwaagatInn";
+const ejsMate = require("ejs-mate");
+const MONGO_URL = "mongodb://127.0.0.1:27017/Undiscover_India";
 
 main()
     .then(() => {
@@ -23,6 +23,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
