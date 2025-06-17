1e586fdd3e2a40be3e7bf13aacad8f127ac9f98a
Subham Kumar Giri <Subhamgiri482@gmail.com>
2025-06-17 12:48:01 +0530

    update

--- models/listing.js
+++ models/listing.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    image: { type: String,
-        default:"https://unsplash.com/photos/mountains-and-lake-with-a-boat-peaceful-scenery-LExlOWiuwR0", set:(v)=>v ==="" ?" " :v, required: true },
-    price: { type: Number, required: true },
-    location: { type: String, required: true },
-    country: { type: String, required: true },
+    title: String,
+    description: String,
+    image: {
+        filename: String,
+        url: String
+    },
+    price: Number,
+    location: String,
+    country: String
+});
 
-})
-
-const Listing = mongoose.model('Listing', listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
