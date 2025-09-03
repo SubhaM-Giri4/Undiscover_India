49739e28bdd6ae49aadc560aeb49cf430c2f8331
Subham Kumar Giri <Subhamgiri482@gmail.com>
2025-09-03 23:57:09 +0530

    added review

--- models/listing.js
+++ models/listing.js
@@ -10,7 +10,14 @@ const listingSchema = new Schema({
     },
     price: Number,
     location: String,
-    country: String
+    country: String,
+
+    reviews :[
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ]
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
