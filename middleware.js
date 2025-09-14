6b00da077001979d880b64e0965d014c6ef11ea6
Subham Kumar Giri <Subhamgiri482@gmail.com>
2025-09-15 02:30:48 +0530

    Authorization

--- middleware.js
+++ middleware.js
@@ -1,8 +1,18 @@
 
-module.exports.isLoggedIn = (req, res) => {
+module.exports.isLoggedIn = (req, res,next) => {
     if(!req.isAuthenticated()){
+        //console.log(req);//show many thing has path and all must read
+        //path has origin URL
+        req.session.redirectUrl = req.originalUrl;//we store the url info
         req.flash("error", "You are not logged in.");
         return res.redirect("/login");
     }
     next();
 };
+
+module.exports.saveRedirectUrl = (req,res,next) => {//we are saving the path as after we have login we have to come to this path ..as it may delete we need to save it
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+}
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
-
+const {savRedirectUrl} = require("../middleware.js");
+const {saveRedirectUrl} = require("../middleware");
 
 router.get("/signup", function (req, res) {
     res.render("users/signup.ejs");
@@ -16,8 +17,14 @@ router.post("/signup",
         const newUser = new User ({username, email});
         const registeredUser = await User.register(newUser, password);
         //console.log(registeredUser);
-        req.flash("success", "Welcome to Swaagatin!");
-        res.redirect("/listings");   // not res.render("/listings")
+        req.login(registeredUser,(err) =>{//this is used as when we do sign up we want to get automatically login so we do this
+            if (err){
+                return next(err);
+            }
+            req.flash("success", "Welcome to Swaagatin!");
+            res.redirect("/listings");   // not res.render("/listings")
+        })
+
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -29,16 +36,28 @@ router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
 
-router.post("/login",
+router.post("/login", saveRedirectUrl,//we save the curr path after login we will come here
     passport.authenticate("local", {
         failureRedirect: "/login",
         failureFlash: true
     }),
     async (req, res) => {
         req.flash("success", "Welcome back!");
-        res.redirect("/listings");
+        let redirectUrl = res.locals.redirectUrl || "/listings";//as it can be empty at when we login from listing page as by islogged in func will help us to save path ..but here it will not call that function only as we are in home page
+        res.redirect(redirectUrl);//we will jump to the path where we enter after login
     }
 );
+
+//to logout user
+router.get("/logout", (req, res,next) => {
+    req.logout((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success", "You are logged out!");
+        res.redirect("/listings");
+    })
+})
 
 
 module.exports = router;
